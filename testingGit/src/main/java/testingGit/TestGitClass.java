6fc7e9a3139c72d107334004b67732a6a2eb8017
Thenaruvip <thenaruvipurushothaman@gmail.com>
2025-04-08 19:40:53 +0530

    new line

---
+++ testingGit/src/main/java/testingGit/TestGitClass.java
@@ -0,0 +1,21 @@
+package testingGit;
+
+import java.util.Scanner;
+
+
+public class TestGitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter text:");
+		String input = sc.nextLine();
+		System.out.println("You entered: " + input);
+		System.out.println("Thank you");
+		//testing the git 
+		
+	}
+
+}
+
+	
