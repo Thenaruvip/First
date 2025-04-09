234116d733b74f1a79801c4f6a42ae4c790d2f5c
Thenaruvip <thenaruvipurushothaman@gmail.com>
2025-04-09 17:41:59 +0530

    operators

--- testingGit/src/main/java/testingGit/TestGitClass.java
+++ testingGit/src/main/java/testingGit/TestGitClass.java
@@ -1,22 +1,64 @@
 package testingGit;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 
 public class TestGitClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter text:");
-		String input = sc.nextLine();
-		System.out.println("You entered: " + input);
-		System.out.println("Thank you");
-		System.out.println("welcome");
-		//testing the git 
 		
-	}
+		int a=10;
+		int b=5;
+		
+		//Arithmetic
+		System.out.println("a+b="+(a+b));
+		System.out.println("a-b="+(a-b));
+		System.out.println("a*b="+(a*b));
+		System.out.println("a/b="+(a/b));
+		System.out.println("a%b="+(a%b));
+
+		//assignement operators
+		int c=10;
+		c += 5;
+		System.out.println("c +="+ c);
+		c -= 3;
+		System.out.println("c -="+ c);
+		c *= 2;
+		System.out.println("c *="+ c);
+		c /= 4;
+		System.out.println("c /="+ c);
+		c %= 5;
+		System.out.println("c %="+ c);
+		
+		//Relational operator/ comparision operator
+		
+		System.out.println("Relational");
+		
+		System.out.println("a==b:"+ (a==b));
+		System.out.println("a!=b:"+ (a!=b));
+		System.out.println("a<b:"+ (a<b));
+		System.out.println("a>b:"+ (a>b));
+		System.out.println("a<=b:"+ (a<=b));
+		
+		// logical operators:
+		System.out.println("Logical-------");
+		System.out.println("a>5 && b>10:"+ (a>5 && b<10));
+		System.out.println("a>5 && b>10:"+ (a>5 || b>10));
+		System.out.println("!(a==b):"+ !(a>b));
+		
+		//Unary operators:
+		int x=10;
+		System.out.println("x:"+x);
+		System.out.println("++x:"+ (++x));
+		System.out.println("++x:"+ (x++));
+		System.out.println("x:"+x);
+		System.out.println("--x:"+ (--x));
+		System.out.println("x--:"+ (x--));
+		System.out.println("x:"+x);
+		System.out.println("x:"+x);
+		
+		
 
 }
-
+}
 	
